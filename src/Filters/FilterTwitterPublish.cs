e57fb27d366f42775c1d947e6f65836b695e0390
pluchinetti <40801333+pluchinetti@users.noreply.github.com>
2018-09-21 21:01:30 -0300

    Added comment in TwitterPublish

--- src/Filters/FilterTwitterPublish.cs
+++ src/Filters/FilterTwitterPublish.cs
@@ -24,6 +24,8 @@ namespace CompAndDel.Filters
             
             PictureProvider pictureProvider = new PictureProvider();
             pictureProvider.SavePicture(image,"TeroATwitter.jpg");
+            // new pictureProvider.SavePicture(image,"TeroATwitter.jpg"); // Esta línea es igual a las anteriores. Resulta
+            // conveniente ya que se utiliza para un método puntual y no interesa quedarse con la referencia al objeto.
 
             var twitter = new TwitterImage(consumerKey, consumerKeySecret, accessToken, accessTokenSecret);
             Console.WriteLine(twitter.PublishToTwitter("¿Cuál hay, G? Ahora con filtros, de nada...",@"TeroATwitter.jpg"));
